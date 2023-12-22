cb72bbda86ffe7ad52bb8a3ce9673d82fe28cbee
Awais Ahmed <awais@99technologies.co>
2023-12-22 15:42:24 +0500

    changes

--- index.js
+++ index.js
@@ -95,9 +95,9 @@ app.listen( process.env.PORT , () => {
   console.log(`webhook is listening on port ${process.env.PORT}`);
 });
 app.get("/webhook", (req, res) => {
-  let mode = req.query("hub.mode");
-  let challenge = req.query("hub.challenge");
-  let token = req.query("hub.verify_token");
+  let mode = req.query["hub.mode"];
+  let challenge = req.query["hub.challenge"];
+  let token = req.query["hub.verify_token"];
 
   if (mode && token) {
     if (mode === "subscribe" && token === mytoken) {
